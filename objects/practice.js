38e2c7c91f31b1fe738f3fef3b760ad355f3a713
bbdoggett3 <bbdoggett3@gmail.com>
2020-04-30 15:21:53 -0600

    I finished the objects section

--- objects/practice.js
+++ objects/practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr) {
+  return arr[0];
+}
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr) {
+  return arr[2];
+}
 
 
 
@@ -47,7 +51,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
   Loop through the given array and alert every item in the array.
 */
 
-//Code Here
+function looper(family) {
+  for(let i = 0; i < family.length; i++) {
+    alert(family[i]);
+  }
+}
+
+// looper(family);
 
 
 
@@ -63,7 +73,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters) {
+  for (let i = letters.length - 1; i >= 0; i--) {
+    alert(letters[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,11 +92,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(nums) {
+  let evenCatcher = [];                        //I created my variable to catch the even numbers in an array.
 
-
-
-
-
+  for(let i = 0; i < nums.length; i++) {       //made a for loop to increment through.
+    if(nums[i] % 2 === 0) {                    //Did the modulus to check if it's even.
+      evenCatcher.push(nums[i]);               //If it is even then I add it my variable evenCatcher's assigned array.
+    }
+  } 
+  return evenCatcher;
+}
 
 
 /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////

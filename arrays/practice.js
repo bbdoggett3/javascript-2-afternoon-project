2693a542664325639aef5351febb8d8acb0bbd9c
bbdoggett3 <bbdoggett3@gmail.com>
2020-04-30 13:55:53 -0600

    half way through with arrays

--- arrays/practice.js
+++ arrays/practice.js
@@ -14,7 +14,10 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+let me = {
+  name: "Ben",
+  age: 27,
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -23,21 +26,30 @@
   Have the values to those keys be strings that are equal to your favorite thing in that category.
 */
 
-//Code here
+let favoriteThings = {
+  band: "The Midnight",
+  food: "Mexican Rice and Beans",
+  person: "Fred Rogers",
+  book: "John Carter",
+  movie: "Tron-Legacy",
+  holiday: "Christmas"
+}
 
 /*
   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
   and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = "Ford";
+favoriteThings.brand = "Nike";
 
 /*
   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets";
+favoriteThings.book = "Harry Potter";
 
 ////////// PROBLEM 3 //////////
 
@@ -48,14 +60,19 @@
   Set the value of that key to 'chapstick'.
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
 */
+item = "firstPocket"
 
-//Code here
+let backPack = {
+  
+}
 
+backPack[item] = "chapstick";
+backPack.color = "Yellow";
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
+// alert(backPack);   REMOVED FOR THE CONSOLE.LOG TO RUN PROPERLY, I DID TRY IT AND SEE IT IN JASMINE.
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -63,7 +80,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console.
 */
 
-//Code here
+console.log(backPack);
 
 ////////// PROBLEM 4 //////////
 
